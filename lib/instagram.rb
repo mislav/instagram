b5283705390ae06d7b24787f0f87733d4a69ed8e
Mislav Marohnić <mislav.marohnic@gmail.com>
2010-11-30 23:47:16 +0100

    main module extends self

--- lib/instagram.rb
+++ lib/instagram.rb
@@ -5,29 +5,36 @@ require 'instagram/models'
 
 module Instagram
   
+  extend self
+  
   Popular = Addressable::URI.parse 'http://instagr.am/api/v1/feed/popular/'
   UserFeed = Addressable::Template.new 'http://instagr.am/api/v1/feed/user/{user_id}/'
   UserInfo = Addressable::Template.new 'http://instagr.am/api/v1/users/{user_id}/info/'
   
-  def self.popular(params = {})
-    url = Popular.dup
-    parse_response(url, params, Timeline)
+  def popular(params = {})
+    parse_response(Popular.dup, params, Timeline)
   end
   
-  def self.by_user(user_id, params = {})
+  def by_user(user_id, params = {})
     url = UserFeed.expand :user_id => user_id
     parse_response(url, params, Timeline)
   end
   
-  def self.user_info(user_id, params = {})
+  def user_info(user_id, params = {})
     url = UserInfo.expand :user_id => user_id
     parse_response(url, params, UserWrap)
   end
   
-  def self.parse_response(url, params, parser)
+  private
+  
+  def parse_response(url, params, parser)
     url.query_values = params
-    body = Net::HTTP.get url
+    body = get_url url
     parser.parse body
+  end
+  
+  def get_url(url)
+    Net::HTTP.get url
   end
   
 end
