249322a134cee92bc00bbaf79d114d23fa8f80de
Mislav Marohnić <mislav.marohnic@gmail.com>
2010-12-01 19:46:03 +0100

    pagination

--- app.rb
+++ app.rb
@@ -31,6 +31,10 @@ helpers do
   def instalink(text)
     text.sub(/\b(instagram)\b/i, '<a href="http://instagr.am">\1</a>')
   end
+  
+  def xhr?
+    !(request.env['HTTP_X_REQUESTED_WITH'] !~ /XMLHttpRequest/i)
+  end
 end
 
 get '/' do
@@ -42,12 +46,16 @@ get '/' do
 end
 
 get '/users/:id' do
-  @user = CachedInstagram::user_info params[:id]
-  @photos = CachedInstagram::by_user @user.id
-  @title = "Photos by #{@user.username} on Instagram"
+  unless xhr?
+    @user = CachedInstagram::user_info params[:id]
+    @title = "Photos by #{@user.username} on Instagram"
+  end
+  
+  feed_params = params[:max_id] ? { max_id: params[:max_id].to_s } : {}
+  @photos = CachedInstagram::by_user(params[:id], feed_params)
   
   expires 30.minutes, :public
-  haml :index
+  haml(xhr? ? :photos : :index)
 end
 
 get '/screen.css' do
@@ -79,18 +87,7 @@ __END__
       followers
 
 %ol#photos
-  - for photo in @photos
-    %li
-      %a{ href: photo.image_url(612), class: 'thumb' }
-        - img(photo, 150)
-      .full{ style: 'display:none' }
-        %img{ width: 480, height: 480 }
-        %h2= photo.caption
-        .author
-          by
-          %a{ href: "/users/#{photo.user.id}" }&= photo.user.full_name
-        .close
-          %a{ href: "#close" } close
+  = haml :photos
 
 %footer
   %p
@@ -111,6 +108,34 @@ __END__
     $(this).closest('li').removeClass('active')
     $('#photos').removeClass('lightbox')
   })
+  
+  $('#photos .pagination a').live('click', function(e) {
+    e.preventDefault()
+    $(this).text('Loading...')
+    var item = $(this).closest('.pagination')
+    $.get($(this).attr('href'), function(body) {
+      item.remove()
+      try { $('#photos').append(body) }
+      catch(e) { $('#photos').get(0).innerHTML += body } // for mozilla
+    })
+  })
+
+@@ photos
+- for photo in @photos
+  %li
+    %a{ href: photo.image_url(612), class: 'thumb' }
+      - img(photo, 150)
+    .full{ style: 'display:none' }
+      %img{ width: 480, height: 480 }
+      %h2= photo.caption
+      .author
+        by
+        %a{ href: "/users/#{photo.user.id}" }&= photo.user.full_name
+      .close
+        %a{ href: "#close" } close
+- if @user and @photos.length >= 20
+  %li.pagination
+    %a{ href: request.path + "?max_id=#{@photos.last.id}" } Load more &rarr;
 
 @@ style
 body {
@@ -145,6 +170,17 @@ p.stats { color: gray; font-style: italic; font-size: 90%; margin-top: -1.1em }
         a { color: white }
         .close { margin-top: -1.15em; text-align: right; width: 480px }
       }
+    }
+    &.pagination {
+      text-align: center;
+      a {
+        margin-left: 1em;
+        font-size: 80%; padding: .2em .6em;
+        color: white; background: #bbb; text-decoration: none;
+        text-shadow: rgba(0,0,0,.4) 1px 1px 1px;
+        position: relative; top: -10px;
+      }
+      a:hover { background: #999 }
     }
     h2 { font-size: 1.2em; margin: .5em 0; }
   }
